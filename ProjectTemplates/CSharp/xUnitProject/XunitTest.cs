21386d270b0fbb660d4fc77145401658f156a663
Michael Helfenstein <m.helfenstein@gmail.com>
2019-01-05 21:46:00 +0100

    #6 - add xunit projecttemplate

---
+++ ProjectTemplates/CSharp/xUnitProject/XunitTest.cs
@@ -0,0 +1,33 @@
+﻿using FakeItEasy;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace $safeprojectname$
+{
+	public class XunitTest
+    {
+        private readonly ITestOutputHelper _testOutput;
+
+        public XunitTest(ITestOutputHelper testOutput)
+        {
+            _testOutput = testOutput;
+        }
+
+        [Fact]
+        public void SomethingShouldBeDone()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+
+        }
+    }
+}
